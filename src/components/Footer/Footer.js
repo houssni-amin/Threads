136694be511a3e17f3dbc50a10ef12274c961885
Amin Houssni <houssni.amin@outlook.fr>
2024-07-10 16:38:15 +0200

    Created footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="mt-10 flex flex-col justify-center gap-1 pb-4 text-xs text-threads-gray-light sm:flex-row sm:gap-10 sm:text-sm">
+      <div className="text-center">© Threads, 2024</div>
+      <div className="text-center">Conditions générales de Threads</div>
+      <div className="text-center">Politique de confidentialité</div>
+    </footer>
+  )
+}
