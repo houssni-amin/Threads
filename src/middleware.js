7309c97c8452e0c76035157db91d0153b3f7ecb8
Amin Houssni <houssni.amin@outlook.fr>
2024-07-12 11:45:41 +0200

    Added guest mode to middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,18 @@
+import { hasCookie } from "cookies-next"
 import { NextResponse } from "next/server"
+import { cookies } from "next/headers"
 
 export function middleware(request) {
   let isAuthenticated = false
 
+  // Check if is invited user
+  if (hasCookie("guest", { cookies })) {
+    isAuthenticated = true
+  }
+
+  // Check if connected
+
+  // Check if isAuthenticated
   if (!isAuthenticated) {
     const url = request.nextUrl.clone()
     url.pathname = "/login"
