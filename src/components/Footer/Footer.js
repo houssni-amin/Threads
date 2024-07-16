54ec71d85ec2d2a6fb6bb831bfe7b19847960d77
Amin Houssni <houssni.amin@outlook.fr>
2024-07-16 16:46:16 +0200

    Improved Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
-export default function Footer({ mb = "" }) {
+export default function Footer({ marginBottom }) {
   return (
     <footer
-      className={`${mb} mt-10 flex flex-col justify-center gap-1 pb-4 text-xs text-threads-gray-light sm:flex-row sm:gap-10 sm:text-sm md:mb-0`}
+      className={`${marginBottom && "mb-[88px]"} mt-10 flex flex-col justify-center gap-1 pb-4 text-xs text-threads-gray-light sm:flex-row sm:gap-10 sm:text-sm md:mb-0`}
     >
       <div className="text-center">© Threads, 2024</div>
       <div className="text-center">Conditions générales de Threads</div>
