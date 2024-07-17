48a0ec8c2dcfb654d1b849972f8da9e88fd31030
Amin Houssni <houssni.amin@outlook.fr>
2024-07-17 20:44:06 +0200

    Added form field validation

--- src/app/login/signup/page.jsx
+++ src/app/login/signup/page.jsx
@@ -1,8 +1,42 @@
+"use client"
+
+import { checkEmailSyntax } from "@/utlis/checkEmailSyntax"
 import Link from "next/link"
+import { toast } from "react-toastify"
+import { createUser } from "@/actions/create-user"
+import { useRouter } from "next/navigation"
 
 export default function Signup() {
+  const router = useRouter()
+
+  const prepareCreateUser = async (FormData) => {
+    const username = FormData.get("username")
+    const pseudo = FormData.get("pseudo")
+    const email = FormData.get("email")
+    const password = FormData.get("password")
+
+    if (!username || !pseudo || !email || !password) {
+      return toast.error("Veuillez remplir tous les champs.")
+    }
+
+    if (!checkEmailSyntax(email)) {
+      return toast.error("Veuillez entre un email valide.")
+    }
+
+    try {
+      await createUser(username, pseudo, email, password)
+    } catch (error) {
+      return toast.error(error.message)
+    }
+
+    toast.success("Inscription réussie !")
+
+    //Redirect
+    router.push("/login/signin")
+  }
+
   return (
-    <div className="mx-auto p-5 pt-[150px] text-white md:max-w-[500px] md:px-0 md:pt-[270px]">
+    <div className="mx-auto max-w-[500px] p-5 pt-[150px] text-white md:px-0 md:pt-[270px]">
       <div className="mb-4 flex">
         <Link href="/login">
           <svg
@@ -20,45 +54,49 @@ export default function Signup() {
         </Link>
         <p className="text-2xl font-bold">Inscrivez-vous</p>
       </div>
-      <input
-        type="text"
-        name="username"
-        placeholder="Nom d'utilisateur"
-        className="info"
-        required
-      />
-      <input
-        type="text"
-        name="pseudo"
-        placeholder="Pseudo"
-        className="info"
-        required
-      />
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        className="info"
-        required
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Mot de passe"
-        className="info"
-        required
-      />
-      <button className="w-full rounded-3xl bg-white py-4 text-black">
-        S&apos;inscrire
-      </button>
+      <form action={prepareCreateUser} noValidate>
+        <input
+          type="text"
+          name="username"
+          placeholder="Nom d'utilisateur"
+          className="info"
+          required
+        />
+        <input
+          type="text"
+          name="pseudo"
+          placeholder="Pseudo"
+          className="info"
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          className="info"
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Mot de passe"
+          className="info"
+          required
+        />
+        <button className="w-full rounded-3xl bg-white py-4 text-black">
+          S&apos;inscrire
+        </button>
+      </form>
       <div className="my-4 flex items-center justify-center">
         <div className="w-1/4 border-t border-threads-gray-light"></div>
         <div className="mx-4 text-threads-gray-light">ou</div>
         <div className="w-1/4 border-t border-threads-gray-light"></div>
       </div>
-      <button className="w-full rounded-3xl bg-white py-4 text-black">
-        Se connecter
-      </button>
+      <Link href="/login/signin">
+        <button className="w-full rounded-3xl bg-white py-4 text-black">
+          Se connecter
+        </button>
+      </Link>
     </div>
   )
 }
