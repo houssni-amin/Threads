eede5fb3637516713f90eb73072c69f850fafaec
Amin Houssni <houssni.amin@outlook.fr>
2024-07-23 16:34:22 +0200

    Updated styles for newPostModal

--- src/components/ConnectedLayout/ConnectedLayout.jsx
+++ src/components/ConnectedLayout/ConnectedLayout.jsx
@@ -3,8 +3,8 @@
 import { useState } from "react"
 import Footer from "../Footer/Footer"
 import Header from "../Header/Header"
-import NewPostForm from "../NewPostForm/NewPostForm"
 import { createPortal } from "react-dom"
+import NewPostFormModal from "../NewPostFormModal/NewPostFormModal"
 
 export default function ConnectedLayout({ children }) {
   const [openModale, setOpenModale] = useState(false)
@@ -14,15 +14,15 @@ export default function ConnectedLayout({ children }) {
       {openModale &&
         createPortal(
           <div
-            className="fixed bottom-0 left-0 right-0 top-0 z-30 flex items-center justify-center bg-gray-900 bg-opacity-80"
+            className="fixed bottom-0 left-0 right-0 top-0 z-30 flex items-center justify-center bg-black bg-opacity-70"
             onClick={(e) => {
               if (e.target === e.currentTarget) {
                 setOpenModale(false)
               }
             }}
           >
-            <div className="w-10/12 overflow-y-auto rounded-xl border border-threads-gray-light bg-threads-modal p-5 shadow lg:w-[740px]">
-              <NewPostForm closeModale={() => setOpenModale(false)} />
+            <div className="w-10/12 rounded-xl border border-threads-gray-light bg-threads-modal p-5 shadow md:max-w-[700px]">
+              <NewPostFormModal closeModale={() => setOpenModale(false)} />
             </div>
           </div>,
           document.body,
---
+++ src/components/NewPostFormModal/NewPostFormModal.jsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { createPost } from "@/actions/create-post"
+import { useSession } from "next-auth/react"
+import Image from "next/image"
+import { useState } from "react"
+import { toast } from "react-toastify"
+
+export default function NewPostFormModal({ closeModale = () => {} }) {
+  const { data: session } = useSession()
+
+  const pseudo = session?.user?.pseudo
+
+  const profilImage = session?.user.profile || "/picture.png"
+
+  const [textarea, setTextarea] = useState("")
+
+  const onPrepare = async (formData) => {
+    try {
+      await createPost(formData)
+      setTextarea("")
+    } catch (error) {
+      return toast.error(error.message)
+    }
+    closeModale()
+  }
+
+  return (
+    <form action={onPrepare} className="mx-auto p-4 md:max-w-[700px] md:px-0">
+      <div className="mb-5 flex">
+        <div className="mr-3 flex max-h-[50px] min-h-[50px] min-w-[50px] max-w-[50px]">
+          <Image
+            src={profilImage}
+            alt="User"
+            width={50}
+            height={50}
+            className="size-full rounded-full"
+          />
+        </div>
+        <div className="text-lg font-semibold text-white">@{pseudo}</div>
+      </div>
+      <div className="flex size-full">
+        <div className="flex w-full">
+          <textarea
+            placeholder="Commencer un thread..."
+            id="content"
+            className="newPostFormTxt"
+            value={textarea}
+            name="content"
+            onChange={(e) => setTextarea(e.target.value)}
+          ></textarea>
+
+          <button className="newPostFormBtn" disabled={textarea === ""}>
+            Publier
+          </button>
+        </div>
+      </div>
+    </form>
+  )
+}
